47e0f82b608eb26b3f28c0c818856c0f6be23741
Aleksandr Sagalajev <ASagalajev92@gmail.com>
2020-11-07 17:24:45 +0200

    Update express,body-parser.js
    Added some requirements for Mongo DB and Mongoose.

--- express,body-parser.js
+++ express,body-parser.js
@@ -1,19 +1,31 @@
-// SETTING UP EXPRESS AND BODY-PARSER
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
-const port = 3000;
 const app = express();
 
-app.get("/", function (req,res) {
-  res.send("Local server is running and is prepared for development !");
+app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+//-----    Connect to our mongo dbs using mongoose package   -----//
+mongoose.connect('mongodb+srv://account:password@cluster0.mzo7m.mongodb.net/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 
+app.get("/", function(req, res) {
+    res.render("home");
+});
 
-app.listen(port, function () {
-  console.log("Server is running on port :" + port);
 
+//-----    Server listening   -----//
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
 });
